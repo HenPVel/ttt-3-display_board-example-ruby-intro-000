7545f5bcd96501dd5412e47fcd7a9c3d5f84f21e
amusing-tutorial-3bc0 <hvelas93@gmail.com>
2021-01-25 19:30:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-m=Matrix[1,2,3],
-        [4,5,6],
-        [7,8,9]
-        
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
